f302e0401411fb927ccc0111632994362424b883
YEYE <151682411+59-59@users.noreply.github.com>
2023-11-21 16:06:05 -0500

    Create config.py

---
+++ Qtile/config.py
@@ -0,0 +1,19 @@
+#Files
+from keys import keys
+from workspaces import groups
+from mouse import mouse
+from widgets import screens,layouts,layout_conf,layout_conf2,widget_defaults,extension_defaults
+from DragFL import floating_layout
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+auto_minimize = True
+wl_input_rules = None
+wmname = "LG3D"
